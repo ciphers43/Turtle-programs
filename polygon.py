d0564ef6fc1d7a49ee252275f18f21e5a9efb62e
ciphers43 <116568242+ciphers43@users.noreply.github.com>
2022-10-24 19:22:09 +0530

    Create polygon.py

---
+++ polygon.py
@@ -0,0 +1,10 @@
+#Python programming to draw polygon in turtle programming
+import turtle
+ 
+t = turtle.Turtle()
+numberOfSides = int(input('Enter the number of sides of a polygon: '))
+lengthOfSide = int(input('Enter the length of a side of a polygon: '))
+exteriorAngle = 360/numberOfSides
+for i in range(numberOfSides):
+   t.forward(lengthOfSide)
+   t.right(exteriorAngle)
