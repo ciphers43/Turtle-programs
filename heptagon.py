41e728c9af3dfa89bdc9a1d2d8ee208c91402de9
ciphers43 <116568242+ciphers43@users.noreply.github.com>
2022-10-24 19:19:50 +0530

    Create heptagon.py

---
+++ heptagon.py
@@ -0,0 +1,7 @@
+#Python programming to draw heptagon in turtle programming
+import turtle
+ 
+t = turtle.Turtle()
+for i in range(7):
+   t.forward(100) #Assuming the side of a heptagon is 100 units
+   t.right(51.42) #Turning the turtle by 51.42 degree
